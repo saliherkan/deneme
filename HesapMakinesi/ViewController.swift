83f0247a1ebf8f8174f61c35b4d1c50f699e06c3
Sefer Furkan Sandal <root@furkansandal.com>
2020-08-16 00:04:49 +0300

    Initial Commit

---
+++ HesapMakinesi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HesapMakinesi
+//
+//  Created by Sefer Furkan Sandal on 16.08.2020.
+//  Copyright © 2020 Sefer Furkan Sandal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
