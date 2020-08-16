021ae99f9c4c56fa5efd68f7686bbf6358875caa
Sefer Furkan Sandal <root@furkansandal.com>
2020-08-16 09:46:28 +0300

    Hata ayıklamalı ilk versiyon

--- HesapMakinesi/ViewController.swift
+++ HesapMakinesi/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ilkFiled: UITextField!
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    @IBOutlet weak var ikinciField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toplamaIslemi(_ sender: Any) {
+        let ilk : Int = Int(ilkFiled.text ?? "1") ?? 0
+        let ikinci : Int = Int(ikinciField.text ?? "1") ?? 0
+        let sonuc : Int = ilk + ikinci
+        
+        sonucLabel.text = "Sonuç: "  + String(sonuc)
+    }
+    @IBAction func cikarmaIslemi(_ sender: Any) {
+        let ilk : Int = Int(ilkFiled.text ?? "1") ?? 0
+        let ikinci : Int = Int(ikinciField.text ?? "1") ?? 0
+        let sonuc : Int = ilk - ikinci
+        
+        sonucLabel.text = "Sonuç: "  + String(sonuc)
+    }
+    @IBAction func carpmaIslemi(_ sender: Any) {
+        let ilk : Int = Int(ilkFiled.text ?? "1") ?? 0
+        let ikinci : Int = Int(ikinciField.text ?? "1") ?? 0
+        let sonuc : Double = Double(ilk) * Double(ikinci)
+        
+        sonucLabel.text = "Sonuç: "  + String(sonuc)
+    }
+    @IBAction func bolmeIslemi(_ sender: Any) {
+        let ilk : Int = Int(ilkFiled.text ?? "1") ?? 0
+        let ikinci : Int = Int(ikinciField.text ?? "1") ?? 0
+        let sonuc : Double = Double(ilk) / Double(ikinci)
+        
+        sonucLabel.text = "Sonuç: "  + String(sonuc)
+    }
 }
 
